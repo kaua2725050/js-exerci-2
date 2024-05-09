aafba0cda34845fc65af6cf7705e381767753e11
KAUA PEREIRA <54439995837@info.local>
2024-05-09 14:37:36 -0300

    fim defin

--- exercicio7.js
+++ exercicio7.js
@@ -3,8 +3,12 @@
 console.log("-------------------------------------")
 
 let numero = 1
+let soma = 0
 
 while (numero <=100) {
     console.log(numero)
-    numero +=1
+    soma += numero
+    numero ++;
 }
+
+console.log("essa é a soma dos números de 1 a 100: " + soma)
